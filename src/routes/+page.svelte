e5e1ef44bb5112984a1d467f01d60d9f36f6b0c6
Pedro Henrique Coterli <pedrocoterli@gmail.com>
2025-03-23 17:35:44 -0300

    Finalizando o lab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+</script>
+
 <h1> Pedro Henrique Coterli </h1>
 
 <!-- <p> (Paragraph) represents a paragraph of text.-->
@@ -5,3 +10,11 @@
 
 <!-- <img> (Image) tag is used to display images.-->
 <img src="images/my_personal_image.png" alt="Me at the Museu do Amanhã." height="400">
+
+<h1> Latest projects </h1>
+
+<div class = "projects">
+    {#each projects.slice(0, 3) as p}
+        <Project data = {p} hLevel = "3" />
+    {/each}
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,23 @@
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+</script>
+
 <svelte:head>
     <title>Projects</title>
 </svelte:head>
 
-<h1>Projects</h1>
+<!-- <pre>
+    {JSON.stringify(projects, null, "\t")}
+</pre> -->
+
+<h1>{projects.length} Projects</h1>
     
 <div class="projects">
-    <article>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <img src="https://i.etsystatic.com/iap/a0f246/5502938083/iap_640x640.5502938083_69958kp0.jpg" alt="">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nesciunt reiciendis possimus dicta doloremque amet sunt aspernatur deserunt sequi! Est hic nesciunt libero natus modi earum maiores aut cum incidunt.</p>
-    </article>
-    <article>
+    {#each projects as p}
+        <Project data = {p} />
+    {/each}
+    <!-- <article>
         <h2>Voluptatibus sint eos consequuntur.</h2>
         <img src="https://i.etsystatic.com/iap/a0f246/5502938083/iap_640x640.5502938083_69958kp0.jpg" alt="">
         <p>Quasi et culpa suscipit, alias quaerat fuga natus eos nulla cupiditate cumque eum voluptatem repellendus dignissimos aut fugiat harum ducimus quibusdam corporis, molestiae expedita ullam hic. Debitis in fugiat nam?</p>
@@ -34,5 +41,5 @@
         <h2>Qui consequuntur dolore quis?</h2>
         <img src="https://i.etsystatic.com/iap/a0f246/5502938083/iap_640x640.5502938083_69958kp0.jpg" alt="">
         <p>Tempore, culpa consequatur voluptatem quam, repellat inventore alias nam ut incidunt obcaecati qui neque eaque illum nostrum totam ipsa quae quaerat sint quis reiciendis omnis natus placeat quibusdam? Ut, assumenda.</p>
-    </article>
+    </article> -->
 </div> 
